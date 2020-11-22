d9f32a8a04152f9028a436be1deabe8f908b5ca4
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2020-11-22 14:36:59 +0100

    Define CLI arguments.
    Change-Id: I37d6cbb000df63013277fe4628cdb6570d68101a

--- src/main/java/eu/mulk/aendggner/AendGgner.java
+++ src/main/java/eu/mulk/aendggner/AendGgner.java
@@ -1,11 +1,23 @@
 package eu.mulk.aendggner;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
-@Command
+@Command(
+    name = "ÄndGgner",
+    mixinStandardHelpOptions = true,
+    version = "ÄndGgner 0.1",
+    description = "Displays German amendment acts in a user-friendly, consolidated way.")
 public class AendGgner implements Callable<Integer> {
+
+  @Parameters(index = "0", description = "The base text to modify.")
+  private File baseFile;
+
+  @Parameters(index = "1", description = "The diff relative to the base text.")
+  private File diffFile;
 
   public static void main(String... args) {
     int exitCode = new CommandLine(new AendGgner()).execute(args);
